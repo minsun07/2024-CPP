8574b68dffd15a24afe918f764214adb4f340c20
An minsun <elleto123456@gmail.com>
2024-09-02 08:42:55 +0900

    02-01 C++ 이름공간

--- 2024-CPP/grammar/02-01기본예제.cpp
+++ 2024-CPP/grammar/02-01기본예제.cpp
@@ -1,9 +1,11 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main(void) {
 
 	// cout: 출력객체, endl: 개행객체
-	std::cout << "Hello World" << std:: endl;
+	cout << "Hello World" << endl;
 	
 	return 0;
 }
