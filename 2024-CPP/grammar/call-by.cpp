ab352d840267a5399fddd111546dd31e72c43788
An minsun <elleto123456@gmail.com>
2024-09-02 03:37:04 +0900

    call by pointer

--- 2024-CPP/grammar/call-by.cpp
+++ 2024-CPP/grammar/call-by.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-void call_by_value(int val)
+void call_by_pointer(int* val)
 {
-	// 새로운 지역변수 val를 만들고 값을 넣는다.
-	val = 10;
+	// 새로운 지역변수 val를 만들고 val가 가리키고 있는 대상의 값을 바꾼다.
+	*val = 10;
 }
 
 int main(void)
 {
 	int num = 4;
-	// num의 값은 바뀌지 않는다.
-	call_by_value(num);
+	// num의 값은 바뀐다.
+	call_by_pointer(&num);
 	cout << num;
 	return 0;
 }
