05475223e2ae018199a2d85d0784c6b268cd4b00
An minsun <elleto123456@gmail.com>
2024-09-08 23:49:28 +0900

    생성자

--- 2024-CPP/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP/grammar/03-01 구조체와 클래스.cpp
@@ -4,14 +4,18 @@
 using namespace std;
 
 class Student {
+public:
+	Student(int hakbun, string name) {
+		hakbun_ = hakbun;
+		name_ = name;
+	}
+private:
 	int hakbun_;
 	string name_;
 };
 
 int main(void) {
-	Student jisu; //클래스는 default가 private이다.
-	
-	jisu.hakbun_ = 2202;
-	jisu.name_ = "나지수";
+	Student jisu = Student(2202, "나지수");
+
 	return 0;
 }
