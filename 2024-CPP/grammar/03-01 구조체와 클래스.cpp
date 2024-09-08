a8d0488afcd1dc5b09887afc4cc87c4d5801f3e6
An minsun <elleto123456@gmail.com>
2024-09-08 23:59:41 +0900

    03-01 생성자 오버로딩

--- 2024-CPP/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP/grammar/03-01 구조체와 클래스.cpp
@@ -7,6 +7,11 @@ class Student {
 public:
 	//생성자 : 객체가 생성횔 때 호출되는 함수
 	//생성자는 반환형이 없다.
+	Student() {
+		hakbun_ =2222;
+		name_ = "swag";
+	}
+	
 	Student(int hakbun, string name) {
 		hakbun_ = hakbun;
 		name_ = name;
@@ -18,5 +23,7 @@ private:
 
 int main(void) {
 	Student jisu = Student(2202, "나지수");
+	Student swag = Student();
+
 	return 0;
 }
