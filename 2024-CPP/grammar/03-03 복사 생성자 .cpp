1819ac26e2b83d94e46b166909b48098ad84ed8a
An minsun <elleto123456@gmail.com>
2024-09-16 21:53:07 +0900

    03-03 얕은복사2

--- 2024-CPP/grammar/03-03 복사 생성자 .cpp
+++ 2024-CPP/grammar/03-03 복사 생성자 .cpp
@@ -1,47 +1,51 @@
 ﻿#include <iostream>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
 class Student {
 public:
-	
-	Student(int hakbun, const char* name) : hakbun_(hakbun)
-	{	
+	Student(int hukbun, const char* name):hukbun_(hukbun) {
 		int length = strlen(name);
 
 		// '\0'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length + 1];
 
 		strcpy(name_, name);
-
 		cout << "생성자 호출 완료" << endl;
 	}
 
+	// 복사 생성자(얕은 복사)
+	Student(const Student& rhs) : hukbun_(rhs.hukbun_), name_(rhs.name_) {
+		cout << "복사 생성자 호출 완료" << endl;
+	}
+
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
-	~Student(void) {
+	~Student() {
+		//TODO : 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
 		delete[]name_;
 		cout << "소멸자 호출 완료" << endl;
 	}
 
 	// 클래스의 멤버를 출력
-	void show(void){
-		cout << hakbun_ << " ";
+	void show() {
+		cout << hukbun_ << endl;
 		cout << name_ << endl;
 	}
 
 private:
-	int hakbun_;
+	int hukbun_;
 	char* name_;
 };
 
-int main(void)
-{
-	Student* stu = new Student(2213, "조수빈");
-	cout << "I'm still hungry -거스 히딩크" << endl;
+int main(void) {
+	// 일반적인 생성자
+	Student stu = Student(2213, "JBS");
 
-	delete stu;
-	cout << "It ain't over till it's over -요기 베라" << endl;
+	// 복사 생성자
+	Student stu2 = stu;
 
+	// TODO: 얕은 복사로 인하여 같은 주소(stu.name_, stu2.name_)에서 두 번 delete시도
+	
 	return 0;
 }
