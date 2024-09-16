a7be1265d37c3977aca8488b1178456624fa1f98
An minsun <elleto123456@gmail.com>
2024-09-16 18:23:00 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
+++ 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
@@ -14,15 +14,21 @@ public:
 		name_ = new char[length + 1];
 
 		strcpy(name_, name);
+
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
-		delete []name_;
+		delete[]name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 
 	// 클래스의 멤버를 출력
-	void show(void){}
+	void show(void){
+		cout << hakbun_ << " ";
+		cout << name_ << endl;
+	}
 
 private:
 	int hakbun_;
@@ -32,8 +38,10 @@ private:
 int main(void)
 {
 	Student* stu = new Student(2213, "조수빈");
+	cout << "I'm still hungry -거스 히딩크" << endl;
 
 	delete stu;
+	cout << "It ain't over till it's over -요기 베라" << endl;
 
 	return 0;
 }
