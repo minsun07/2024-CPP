ddf2b7271fd6fd33fa67024f478eaef537238c21
An minsun <elleto123456@gmail.com>
2024-09-16 18:09:36 +0900

    03-02 소멸자

--- 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
+++ 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
@@ -1,50 +1,39 @@
 ﻿#include <iostream>
-#include <string>
+#include <string.h>
+
 using namespace std;
 
 class Student {
 public:
-	// 생성자 : 객체가 생성될 때 호출되는 함수
-	// 생성자는 반환형이 없다.
-	Student()
-	{	//this : 자기자신을 가리키는 포인터
-		//멤버를 명확하게 가리킬 수 있다.
-		this->hakbun_ = 2222;
-		name_ = "swag";
+	
+	Student(int hakbun, const char* name) : hakbun_(hakbun)
+	{	
+		int length = strlen(name);
+
+		// '\0'가 들어가기 위해 공간 하나 더 추가
+		name_ = new char[length + 1];
+
+		strcpy(name_, name);
 	}
 
-	//멤버변수 초기화 : const/참조 멤버변수를 초기화 할 수 있다.
-	Student(int hakbun, string name):hakbun_(hakbun), name_(name){
-		
+	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
+	~Student(void) {
+		delete []name_;
 	}
 
 	// 클래스의 멤버를 출력
-	void show(void)
-	{
-		cout << hakbun_ << " ";
-		cout << name_ << endl;
-	}
+	void show(void){}
 
 private:
 	int hakbun_;
-	string name_;
+	char* name_;
 };
 
 int main(void)
 {
-	// 동적할당 : 메모리는 heap에서 할당. 실행(런타임) 시 메모리 크기가 정해짐
-	Student* stu = new Student[3]{
-		{2202, "나지수"},
-		{2203, "박지영"},
-		{2215, "추다율"}
-	};
+	Student* stu = new Student(2213, "조수빈");
 
-	for (int i = 0; i < 3; i++) {
-		stu[i].show();
-	}
-
-	//배열의 동적할당 해제
-	delete[]stu;
+	delete stu;
 
 	return 0;
 }
