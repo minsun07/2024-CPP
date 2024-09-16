ec5cd8f3b16884bab52bfc93ddb55580df85bb07
An minsun <elleto123456@gmail.com>
2024-09-16 22:18:33 +0900

    03-03 깊은복사

--- 2024-CPP/grammar/03-03 복사 생성자 .cpp
+++ 2024-CPP/grammar/03-03 복사 생성자 .cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class Student {
 public:
+	
+	// 생성자
 	Student(int hukbun, const char* name):hukbun_(hukbun) {
 		int length = strlen(name);
 
@@ -15,15 +17,26 @@ public:
 		cout << "생성자 호출 완료" << endl;
 	}
 
-	// 복사 생성자(얕은 복사)
-	Student(const Student& rhs) : hukbun_(rhs.hukbun_), name_(rhs.name_) {
+	// 복사 생성자(깊은 복사)
+	Student(const Student& rhs) : hukbun_(rhs.hukbun_){
+
+		int length = strlen(rhs.name_);  // 1. 원본 객체의 문자열 길이 계산
+										 // rhs는 복사될 원본 객체
+
+		
+		name_ = new char[length + 1]; // 2. 새로운 메모리 블록 할당
+									  // '\0'가 들어가기 위해 공간 하나 더 추가
+									  // 문자열 길이 계산 시 널 종료 문자(\0)는 제외됨
+		
+
+		strcpy(name_, rhs.name_);     // 3. 원본 객체의 문자열을 새로운 메모리에 복사
+
 		cout << "복사 생성자 호출 완료" << endl;
 	}
 
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
 	~Student() {
-		//TODO : 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
-		delete[]name_;
+		delete []name_;
 		cout << "소멸자 호출 완료" << endl;
 	}
 
@@ -45,7 +58,6 @@ int main(void) {
 	// 복사 생성자
 	Student stu2 = stu;
 
-	// TODO: 얕은 복사로 인하여 같은 주소(stu.name_, stu2.name_)에서 두 번 delete시도
 	
 	return 0;
 }
