8f70b5daa93f6d2ebaece7ff80cb377dfb466a9b
An minsun <elleto123456@gmail.com>
2024-09-16 22:50:10 +0900

    05-01 부모/자식 생성/소멸자 호출 순서

--- 2024-CPP/grammar/05-01 상속기본.cpp
+++ 2024-CPP/grammar/05-01 상속기본.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 class Person {
 public:
-	Person(string name, int age) : name_(name), age_(age){ }
+	Person(string name, int age) : name_(name), age_(age){
+		cout << "부모 생성자 호출" << endl;
+	}
+
+	~Person() {
+		cout << "부모 소멸자 호출" << endl;
+	}
 
 	void show_person() {
 		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
@@ -20,10 +26,16 @@ class Student : public Person {
 public:
 
 	// 부모 생성자가 먼저 호출
-	Student(string name, int age, int id) :Person(name, age), id_(id) {	}
+	Student(string name, int age, int id) :Person(name, age), id_(id) {	
+		cout << "자식 생성자 호출" << endl;
+	}
+
+	~Student() {
+		cout << "자식 소멸자 호출" << endl;
+	}
 
 	void show_person() {
-		cout << id_ << endl;
+		cout << id_ << " ";
 		Person::show_person(); // 부모 클래스의 멤버함수 호출
 	}
 private:
@@ -33,5 +45,7 @@ private:
 int main() {
 	Student* stu = new Student("조드리헵번", 39, 1004);
 	stu->show_person();
+	delete stu;
+
 	return 0;
 }
