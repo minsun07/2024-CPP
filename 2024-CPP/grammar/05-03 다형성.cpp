cdf204c05be6f8c9a9c26d1eca3cbc6eecf8103a
An minsun <elleto123456@gmail.com>
2024-09-19 20:54:23 +0900

    생성자 복습

--- 2024-CPP/grammar/05-03 다형성.cpp
+++ 2024-CPP/grammar/05-03 다형성.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 class Animal {
 public:
+
+	Animal(string name, unsigned int age, int leg_num)
+		:name_(name),age_(age),leg_num_(leg_num){
+		
+		cout << "이름 : " << name_ << endl;
+		cout <<"나이 : " <<  age_ << endl;
+		cout << "다리 수 : " << leg_num_ << endl;
+	}
+
 	void walk(void) {
 		cout << "걷다" << endl;
 	}
@@ -23,7 +32,7 @@ private:
 };
 
 int main(void) {
-	Animal* animal = new Animal();
+	Animal* animal = new Animal("요아조비",8,2);
 	animal->walk();
 	animal->bark();
 	animal->eat();
