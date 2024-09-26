45c788900425749bafe1ae30fc2f910d285b656f
An minsun <elleto123456@gmail.com>
2024-09-26 11:15:13 +0900

    09-01 좀 더 명확한 enum(중복방지)

--- 2024-CPP/grammar/09-01 enum.cpp
+++ 2024-CPP/grammar/09-01 enum.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // 열거형 정의
-enum Style {	// 0: 한식, 1: 일식, 2: 중식
+enum FoodStyle {	// 0: 한식, 1: 일식, 2: 중식
 	Korean,  // 0 : 디폴트 값이고 값을 주고 싶으면  KOREAN=0으로 한다
 	Japanese,
 	Chinese
@@ -12,7 +12,7 @@ enum Style {	// 0: 한식, 1: 일식, 2: 중식
 void main(void) {
 
 	// 열거형을 사용하면 가독성도 좋아지고, 중간에 값을 유연하게 추가할 수 있다.
-	int style = Korean;
+	int style = FoodStyle::Korean;
 
 	switch (style) {
 		case 0: 
