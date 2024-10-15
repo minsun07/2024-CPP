044842cecc7605da9c8caf5ada26703845a9496f
An minsun <elleto123456@gmail.com>
2024-10-15 14:38:56 +0900

    09-03 auto

---
+++ 2024-CPP/grammar/09-03 auto.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void main(void) {
+	vector<string> v = { "롯데관광", "동화면세점", "제주드림타워", "마이데일리" };
+
+	for (string s : v) {
+		cout << s << endl;
+	}
+}
