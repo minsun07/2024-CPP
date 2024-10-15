627735260648140fe7e20274047829824d2b887a
An minsun <elleto123456@gmail.com>
2024-10-15 14:24:22 +0900

    09-02 범위기반 for문 적용

---
+++ 2024-CPP/grammar/09-02 범위기반 for문.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main(void) {
+	/*int arr[5] = {445, 14, 27, 456, 23};
+	int i;
+	for (i = 0; i < 5; i++) {
+		cout << arr[i] << endl;
+	}*/
+
+	vector <int> v = { 445, 14, 27, 456, 23 };
+
+	/*for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}*/
+
+	for (int value : v) {
+		cout << value << endl;
+	}
+	
+
+}
